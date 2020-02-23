635a47e1e048a48539a252c48e5021e1b23cc893
jai555 <jayavigneshvicky@gmail.com>
2020-02-23 10:29:40 +0530

    till angular directives

---
+++ src/app/product/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+ selector:'pm-products',
+ templateUrl:'./product-list.component.html'
+
+})
+export class ProductListComponent
+{
+    ProductList : String ="Product List by JAaa"
+}
